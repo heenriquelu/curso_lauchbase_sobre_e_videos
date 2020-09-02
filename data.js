9f2021b393e51d18ca4db85859105e1303aff3ce
Luiz Henrique <luizh_17@hotmail.com>
2020-09-02 12:49:04 -0300

    projeto lauchbase

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: "CduBoPGL-Eg",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "hphUPvuyURc",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "b82b73v8HwE",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "3nhheTrwbBY",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id: "t63d3DTypaU",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "LXrN7fAqFgQ",
+    title: "Formulário Animado com JS puro e CSS Animation | Luiz Henrique",
+    duration: "57 min",
+    price: "Free"
+  }
+]
